7c062c4da3827528e1bc61b0af405e41053084f3
silvanocaina <silvanocaina@gmail.com>
2026-07-22 16:43:32 -0300

    Melhorei a Documentação e Adcionei o metodo para remover produtos do carrinho

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,16 @@
+/**
+ * Salvar a lista de produtos no Carrinho
+ * @param {Array} cart O Carrinho
+ */
 function setCart(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+
+/**
+ * Tentar obter o Carrinho
+ * @returns {Array|null} Tenta retornar o Carrinho
+ */
 function getCart() {
   try {
     const json_data = localStorage.getItem("cart");
@@ -11,9 +20,10 @@ function getCart() {
   }
 }
 
+
 /**
- *
- * @returns {Array}
+ * Obtém a lista de produtos do Carrinho ou Criar uma
+ * @returns {Array} A Lista de Produtos no Carrinho
  */
 function getOrCreateCart() {
 
@@ -35,12 +45,16 @@ function getOrCreateCart() {
 
 }
 
+
+/**
+ * Tenta adicionar produto no Carrinho
+ * @param {string|number} productID ID do Produto
+ * @param {number} amount Quantidade a acrescentar
+ * @returns {boolean} Se a operação deu certo
+ */
 function addProductInCart(productID, amount) {
   try {
     const data = getOrCreateCart();
-
-    if (!Array.isArray(data)) return false;
-
 
     const index = data.findIndex((value) => { return value.productID == productID });
     if (index != -1) {
@@ -58,6 +72,45 @@ function addProductInCart(productID, amount) {
   }
 }
 
+/**
+ * Tentar remover Produto no Carrinho
+ * @param {string|number} productID ID do Produto
+ * @param {*} amount Quantidade a reduzir
+ * @returns
+ */
+function removeProductInCart(productID, amount) {
+  try {
+    const data = getOrCreateCart();
+
+    const index = data.findIndex((value) => { return value.productID == productID });
+
+    if (index != -1) { // Se estiver no Carrinho
+
+      // Reduz de forma segura
+      if (data[index].amount > amount) {
+        // remove uma quantidade
+        data[index].amount -= amount;
+      } else {
+        // remove da lista
+        data = data.splice(index, 1);
+    }
+
+    }
+    else { // Se não estiver no carrinho
+      return false;
+    }
+
+    setCart(data);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Obtém a soma total dos Produtos no Carrinho
+ * @returns {number} Retornar o total de produtos no Carrinho
+ */
 function getTotalProductsInCart() {
   const cart = getOrCreateCart();
 
