719f3131141572e3d312ecb88e56ba13ae76ecec
silvanocaina <silvanocaina@gmail.com>
2026-07-25 20:03:12 -0300

    Movendo a logica do botão de carrinho do cabeçalho para header.js

--- pages/header.js
+++ pages/header.js
@@ -5,3 +5,23 @@ if (name_element) {
 
   name_element.innerText = getUsername()
 }
+
+function uptade_cart_info() {
+  const totalProductsInCart = getTotalProductsInCart();
+  cart.innerText = `🛒 ${totalProductsInCart < 100? totalProductsInCart : '99+'}`
+
+  cart.style.display = totalProductsInCart > 0 ? "" : "none";
+}
+
+const cart = document.getElementById('cart-button')
+if (cart) {
+
+
+  // Adicionar evento de click para o botão de carrinho, onde redirecionar para página de carrinho
+  cart.addEventListener('click', () => {
+    // triar para página de carrinho se a autenticação for valida
+    triage('cart.html', 'auth.html')
+  })
+
+  uptade_cart_info()
+}
